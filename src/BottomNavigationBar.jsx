1edff41489880885d1e4558f09013e3e0701b3ce
Vivek-4321 <vivekvenugopal514@gmail.com>
2024-03-05 18:39:22 +0530

    some routes included..

--- src/BottomNavigationBar.jsx
+++ src/BottomNavigationBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./BottomNavigationBar.css";
+import { Link } from 'react-router-dom';
 
 function BottomNavigationBar() {
   return (
     <div className="bottom-navigation-bar">
-    <div className="nav-item">Item 1</div>
-    <div className="nav-item">Item 2</div>
-    <div className="nav-item">Item 3</div>
-    <div className="nav-item">Item 4</div>
+    <Link to="/login"><div className="nav-item">Item 1</div></Link>
+    <Link to="/signup"><div className="nav-item">Item 2</div></Link>
+    <Link to="/loading"><div className="nav-item">Item 3</div></Link>
+    <Link to="/otp_verification"><div className="nav-item">Item 4</div></Link>
   </div>
   )
 }
