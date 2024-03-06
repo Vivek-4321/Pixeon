dd64639b8bf44ff8c4f7cbf8c32cb98299aefe9d
Vivek-4321 <vivekvenugopal514@gmail.com>
2024-03-06 05:44:00 +0530

    NavLink is added

--- src/BottomNavigationBar.jsx
+++ src/BottomNavigationBar.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import "./BottomNavigationBar.css";
-import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import { AiOutlineMessage } from "react-icons/ai";
 import { FiHome } from "react-icons/fi";
 import { MdOutlineSettings } from "react-icons/md";
 import { IoSearchOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
+import "./BottomNavigationBar.css";
 
 function BottomNavigationBar() {
   return (
     <div className="bottom-navigation-bar">
-    <Link className="nav-item-link" to="/"><div className="nav-item"><AiOutlineMessage/></div></Link>
-    <Link className="nav-item-link" to="/loading"><div className="nav-item"><MdOutlineSettings/></div></Link>
-    <Link className="nav-item-link active" to="/signup"><div className="nav-item"><FiHome/></div></Link>
-    <Link className="nav-item-link" to="/otp_verification"><div className="nav-item"><IoSearchOutline/></div></Link>
-    <Link className="nav-item-link" to="/login"><div className="nav-item"><CgProfile/></div></Link>
-  </div>
-  )
+      <NavLink className="nav-item-link" activeClassName="active" to="/">
+        <div className="nav-item">
+          <AiOutlineMessage />
+        </div>
+      </NavLink>
+      <NavLink className="nav-item-link" activeClassName="active" to="/loading">
+        <div className="nav-item">
+          <MdOutlineSettings />
+        </div>
+      </NavLink>
+      <NavLink className="nav-item-link" activeClassName="active" to="/signup">
+        <div className="nav-item">
+          <FiHome />
+        </div>
+      </NavLink>
+      <NavLink
+        className="nav-item-link"
+        activeClassName="active"
+        to="/otp_verification"
+      >
+        <div className="nav-item">
+          <IoSearchOutline />
+        </div>
+      </NavLink>
+      <NavLink className="nav-item-link" activeClassName="active" to="/login">
+        <div className="nav-item">
+          <CgProfile />
+        </div>
+      </NavLink>
+    </div>
+  );
 }
 
 export default BottomNavigationBar;
